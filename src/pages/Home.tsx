317120c9054c9bbede3754873c6f961faa467735
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-08-31 21:57:21 +0900

    [#2] style : mixins, module 추가 및 자잘한변경

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Layout from 'layout';
+import TodoCreator from "components/TodoCreator";
 
 const Home: React.FC = () => {
 	return (
 		<Layout>
 			<section className='section'>
+				<TodoCreator />
 			</section>
 		</Layout>
 	);
