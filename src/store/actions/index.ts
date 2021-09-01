a0cd0071b88443608a146e73614da1ed1638222e
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-09-01 22:26:01 +0900

    [#3] feat : 액션 생성함수 & 액션 상수 추가

---
+++ src/store/actions/index.ts
@@ -0,0 +1,21 @@
+import {
+  addTodoItem,
+  deleteTodoItem,
+  editTodoItem,
+  checkTodoItem,
+  selectTodoItem,
+  unselectTodoItem,
+} from './todos';
+
+import type { TodoAction } from './todos';
+
+export {
+  addTodoItem,
+  deleteTodoItem,
+  editTodoItem,
+  checkTodoItem,
+  selectTodoItem,
+  unselectTodoItem,
+};
+
+export type { TodoAction };
---
+++ src/store/actions/todos.ts
@@ -0,0 +1,52 @@
+import * as types from 'store/actions/actionTypes';
+import { Todo } from 'types/todos';
+
+export const addTodoItem = (payload: Todo) => {
+  return <const>{
+    type: types.ADD_TODO_ITEM,
+    payload,
+  };
+};
+
+export const deleteTodoItem = (payload: Todo['id']) => {
+  return <const>{
+    type: types.DELETE_TODO_ITEM,
+    payload,
+  };
+};
+
+export const editTodoItem = (payload: Todo) => {
+  return <const>{
+    type: types.EDIT_TODO_ITEM,
+    payload,
+  };
+};
+
+export const checkTodoItem = (payload: Todo['id']) => {
+  return <const>{
+    type: types.CHECK_TODO_ITEM,
+    payload,
+  };
+};
+
+export const selectTodoItem = (payload: Todo['id']) => {
+  return <const>{
+    type: types.SELECT_TODO_ITEM,
+    payload,
+  };
+};
+
+export const unselectTodoItem = (payload: Todo['id']) => {
+  return <const>{
+    type: types.UNSELECT_TODO_ITEM,
+    payload,
+  };
+};
+
+export type TodoAction =
+  | ReturnType<typeof addTodoItem>
+  | ReturnType<typeof deleteTodoItem>
+  | ReturnType<typeof editTodoItem>
+  | ReturnType<typeof checkTodoItem>
+  | ReturnType<typeof selectTodoItem>
+  | ReturnType<typeof unselectTodoItem>;
