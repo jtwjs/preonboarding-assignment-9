15fb2b825c01364fabc43ea98296b2336ff14662
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-09-01 22:27:37 +0900

    [#3] feat : 스토어 생성 및 프로바이더 연결

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import store from 'store';
 
 import 'styles/main.scss';
+import App from 'App';
 
-import App from './App';
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById('root'),
 );
---
+++ src/store/index.ts
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import reducers from 'store/reducers';
+
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(logger)),
+);
+
+export default store;
