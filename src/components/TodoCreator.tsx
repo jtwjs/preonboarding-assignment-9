784bca37fe2db1b2dd6efeeb016a99cb2d267102
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-09-01 22:30:21 +0900

    [#3] style : 추가 삭제버튼 로직 & 스타일 변경

--- src/components/TodoCreator.tsx
+++ src/components/TodoCreator.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+import { RootState } from 'store/reducers';
 
 import TodoInput from 'components/TodoInput';
 import TodoPriority from 'components/TodoPriority';
 import Button from 'components/common/Button';
 
-const TodoCreator: React.FC = (props) => {
-	// TODO Delete Btn
+const TodoCreator: React.FC = () => {
+  const { selectedTodos } = useSelector(({ todos }: RootState) => ({
+    selectedTodos: todos.selectedTodos,
+  }));
   return (
     <section className="todo-creator">
       <h3 className="a11y">Todo Creator</h3>
       <TodoInput />
       <TodoPriority />
-      <Button className="add-btn" onClick={() => console.log('서브밋')}>
-        Add
-      </Button>
+      {selectedTodos.length ? (
+        <Button className="btn btn-delete" onClick={() => console.log('삭제')}>
+          Delete
+        </Button>
+      ) : (
+        <Button className="btn btn-add" onClick={() => console.log('추가')}>
+          Add
+        </Button>
+      )}
     </section>
   );
 };
