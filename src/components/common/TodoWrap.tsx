d5348fcb208e8c196bce7d3f297df72728f7119d
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-09-05 20:30:13 +0900

    [#4] feat : drag & drop 구현

--- src/components/common/TodoWrap.tsx
+++ src/components/common/TodoWrap.tsx
@@ -1,14 +1,78 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { TargetIdRef } from 'types/todos';
+import { checkTodoRequestAction, setTodoRequestAction } from 'store/actions';
+import { RootState } from 'store/reducers';
 
 type TodoWrapProps = {
-  title: string;
+  category: string;
+  dragTargetId: { current: TargetIdRef };
+  dropTargetId: { current: TargetIdRef };
+  handleDropped: (isDropped: boolean) => void;
 };
 
-const TodoWrap: React.FC<TodoWrapProps> = ({ children, title }) => {
+const TodoWrap: React.FC<TodoWrapProps> = ({
+  children,
+  category,
+  dragTargetId,
+  dropTargetId,
+  handleDropped,
+}) => {
+  const dispatch = useDispatch();
+  const { todos } = useSelector(({ todos }: RootState) => ({
+    todos: todos.todos,
+  }));
+
+  const isDifferentCategory = (category: string, isCheck: boolean): boolean => {
+    const isDone = category.toLowerCase() === 'done';
+
+    return isDone !== isCheck;
+  };
+
+  const handleDrop = (e: React.DragEvent<HTMLUListElement>) => {
+    const { id: dragId, isCheck } = dragTargetId.current;
+    const { id: dropId } = dropTargetId.current;
+    e.preventDefault();
+
+    if (isDifferentCategory(category, isCheck)) {
+      dispatch(checkTodoRequestAction(dragId));
+    }
+
+    if (!dropId) {
+      return;
+    }
+    if (dragId && dragId !== dropId) {
+      const dragTargetIdx = todos.findIndex((todo) => todo.id === dragId);
+      const dropTargetIdx = todos.findIndex((todo) => todo.id === dropId);
+      const dragTarget = todos.splice(dragTargetIdx, 1);
+      if (dragTargetIdx > dropTargetIdx) {
+        dropTargetIdx === 0
+          ? todos.unshift(dragTarget[0])
+          : todos.splice(dropTargetIdx - 1, 0, dragTarget[0]);
+      } else {
+        todos.splice(dropTargetIdx, 0, dragTarget[0]);
+      }
+      dispatch(setTodoRequestAction(todos));
+    }
+
+    handleDropped(false);
+  };
+
+  const handleDragOver = (e: React.DragEvent<HTMLUListElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <section className="todo-wrap">
-      <h3 className="todo-wrap-title">{title}</h3>
-      <ul className="todo-wrap-list">{children}</ul>
+      <h3 className="todo-wrap-title">{category}</h3>
+      <ul
+        className="todo-wrap-list"
+        onDrop={handleDrop}
+        onDragOver={handleDragOver}
+      >
+        {children}
+      </ul>
     </section>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useRef, useState, useCallback } from 'react';
 import { useSelector } from 'react-redux';
 
 import { RootState } from 'store/reducers';
+import type { TargetIdRef } from 'types/todos';
 
 import Layout from 'layout';
 import TodoCreator from 'components/TodoCreator';
@@ -10,9 +11,21 @@ import TodoWrap from 'components/common/TodoWrap';
 import TodoItem from 'components/common/TodoItem';
 
 const Home: React.FC = () => {
+  const [isDropped, setIsDropped] = useState<boolean>(false);
+  const [mute, setMute] = useState<boolean>(false);
   const { todos } = useSelector(({ todos }: RootState) => ({
     todos: todos.todos,
   }));
+  const dragTargetId = useRef<TargetIdRef>({ id: 0, isCheck: false });
+  const dropTargetId = useRef<TargetIdRef>({ id: 0, isCheck: false });
+
+  const handleMute = useCallback((isMute: boolean) => {
+    setMute(isMute);
+  }, []);
+
+  const handleDropped = useCallback((isDropped) => {
+    setIsDropped(isDropped);
+  }, []);
 
   const todoInProgress = todos.map(
     (todo) =>
@@ -22,6 +35,13 @@ const Home: React.FC = () => {
           id={todo.id}
           priority={todo.priority}
           contents={todo.content}
+          isCheck={todo.isCheck}
+          mute={mute}
+          isDropped={isDropped}
+          handleMute={handleMute}
+          handleDropped={handleDropped}
+          dragTargetId={dragTargetId}
+          dropTargetId={dropTargetId}
         />
       ),
   );
@@ -34,6 +54,13 @@ const Home: React.FC = () => {
           id={todo.id}
           priority={todo.priority}
           contents={todo.content}
+          isCheck={todo.isCheck}
+          mute={mute}
+          isDropped={isDropped}
+          handleMute={handleMute}
+          handleDropped={handleDropped}
+          dragTargetId={dragTargetId}
+          dropTargetId={dropTargetId}
         />
       ),
   );
@@ -47,8 +74,22 @@ const Home: React.FC = () => {
           <TodoProgress />
         </div>
         <div className="content-bottom">
-          <TodoWrap title="In-Progress">{todoInProgress}</TodoWrap>
-          <TodoWrap title="Done">{todoDone}</TodoWrap>
+          <TodoWrap
+            category="In-Progress"
+            handleDropped={handleDropped}
+            dragTargetId={dragTargetId}
+            dropTargetId={dropTargetId}
+          >
+            {todoInProgress}
+          </TodoWrap>
+          <TodoWrap
+            category="Done"
+            handleDropped={handleDropped}
+            dragTargetId={dragTargetId}
+            dropTargetId={dropTargetId}
+          >
+            {todoDone}
+          </TodoWrap>
         </div>
       </section>
     </Layout>
