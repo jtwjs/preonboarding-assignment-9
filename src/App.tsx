0fe55b410877dc019c790a752beaa1541e3508e4
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-09-05 20:25:41 +0900

    [#7] style : 모바일 100vh 적용, drag & drop 스타일 추가 및 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Home from 'pages/Home';
 
 const App: React.FC = () => {
+  useEffect(() => {
+    const vh = window.innerHeight * 0.01;
+    console.log(vh);
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }, []);
+
   return <Home />;
 };
 
